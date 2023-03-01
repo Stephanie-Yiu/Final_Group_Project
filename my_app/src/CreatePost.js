18ac167cf84eec381d6dbcc5fca9a3dafb84fa00
Stephanie-Yiu <116084131+Stephanie-Yiu@users.noreply.github.com>
2023-03-01 14:06:32 +0800

    with create post form

--- my_app/src/CreatePost.js
+++ my_app/src/CreatePost.js
@@ -2,28 +2,65 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
+import { useState } from 'react';
+const modules = {
+  toolbar: [
+    [{ header: [1, 2, false] }],
+    [
+      'bold',
+      'italic',
+      'underline',
+      'strike',
+      'blockquote',
+    ],
+    [
+      { list: 'ordered' },
+      { list: 'bullet' },
+      { indent: '-1' },
+      { indent: '+1' },
+    ],
+    ['link', 'image'],
+    ['clean'],
+  ],
+};
+const formats = [
+  'header',
+  'bold',
+  'italic',
+  'underline',
+  'strike',
+  'blockquote',
+  'list',
+  'bullet',
+  'indent',
+  'link',
+  'image',
+];
 
 export default function CreatePost() {
-con
+  const [title, setTitle] = useState('');
+  const [summary, setSummary] = useState('');
+  const [content, setContent] = useState('');
+  const [files, setFiles] = useState('');
 
+  function createNewPost(e) {
+    const data = new FormData();
+    data.set('title', title);
+    data.set('summary', summary);
+    data.set('content', content);
+    // data.set('file');
 
+    e.preventDefault();
+    console.log(files);
+    
+    // fetch(
+    //   'http://localhost:4000/creat',
+    //   {
+    //     method: 'POST',
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    //   }
+    // )
+  }
 
   return (
     <div className="container-fluid text-center w-100 mt-4">
@@ -33,8 +70,7 @@ con
         style={{ maxWidth: '600px' }}>
         <div className="row">
           <div className="col-12">
-            <Form>
-              {/*onSubmit={handleSubmit}*/}
+            <Form onSubmit={createNewPost}>
               <Form.Group
                 className="mb-3 text-start"
                 controlId="title">
@@ -47,6 +83,28 @@ con
                   style={{
                     background: '#d4d4d4',
                   }}
+                  value={title}
+                  onChange={e =>
+                    setTitle(e.target.value)
+                  }
+                />
+              </Form.Group>
+              <Form.Group
+                className="mb-3 text-start"
+                controlId="title">
+                <Form.Label className="green fw-bold ">
+                  Summary:
+                </Form.Label>
+                <Form.Control
+                  type="text"
+                  className="rounded-0"
+                  style={{
+                    background: '#d4d4d4',
+                  }}
+                  value={summary}
+                  onChange={e =>
+                    setSummary(e.target.value)
+                  }
                 />
               </Form.Group>
 
@@ -54,24 +112,32 @@ con
                 className="mb-3 text-start"
                 controlId="formFile">
                 <Form.Label className="green fw-bold">
-                  Default Image file :
+                  Default file :
                 </Form.Label>
-                <Form.Control type="file" />
+                <Form.Control
+                  type="file"
+                  onChange={e =>
+                    setFiles(e.target.v)
+                  }
+                />
               </Form.Group>
 
               <Form.Group
                 className="mb-3 text-start"
                 controlId="body">
                 <Form.Label className="green fw-bold">
-                  body:{' '}
+                  Content:
                 </Form.Label>
-
                 <ReactQuill
-                  theme="snow"
+                  value={content}
+                  onChange={newValue =>
+                    setContent(newValue)
+                  }
+                  modules={modules}
+                  formats={formats}
                   style={{
                     background: '#d4d4d4',
                   }}
-                  type="body"
                 />
               </Form.Group>
 
