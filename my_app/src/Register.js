e1e76a8581b6d87205d27ae1eb2794af7615cb37
Joanna <Joanna@fakemail.com>
2023-03-01 21:59:30 +0800

    edit register css

--- my_app/src/Register.js
+++ my_app/src/Register.js
@@ -1,119 +1,94 @@
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
-import Button from 'react-bootstrap/esm/Button';
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import Button from "react-bootstrap/esm/Button";
+import Form from "react-bootstrap/Form";
 // import fetch from 'node-fetch'
 // import { useNavigate } from 'react-router-dom';
 
 export default function Register() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [email, setEmail] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
 
   async function handleSubmitRegister(e) {
     e.preventDefault();
 
-    const response = await fetch(
-      'http://localhost:4000/register',
-      {
-        method: 'POST',
-        body: JSON.stringify({
-          username,
-          password,
-          email,
-        }),
-        headers: {
-          'Content-Type': 'application/json',
-        },
+    const response = await fetch("http://localhost:4000/register", {
+      method: "POST",
+      body: JSON.stringify({
+        username,
+        password,
+        email,
+      }),
+      headers: {
+        "Content-Type": "application/json",
       },
-    );
+    });
     console.log(response);
     if (response.status === 200) {
-      alert('registeration  successful');
+      alert("registeration  successful");
     } else {
-      alert('registeration failed');
+      alert("registeration failed");
     }
   }
 
   return (
-    <div className="container-fluid text-start green logincontainer w-50 py-5">
-      <h2 className="d-flex justify-content-center p-3 fw-bold">
-        Create an account
-      </h2>
+    <div className="container-fluid text-start green logincontainer">
+      <h1 className="d4d4 fw-bold about-title text-center mt-5 py-5 display-5 ">
+        {"< Register />"}
+      </h1>
+      <div className="container" style={{ maxWidth: "500px" }}>
+        <Form onSubmit={handleSubmitRegister} style={{ maxWidth: "100%" }}>
+          <Form.Group className="mb-3" controlId="name">
+            <Form.Label className="green fw-bold">Your Name</Form.Label>
+            <Form.Control
+              name="name"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              type="text"
+              className="rounded-0 name"
+              style={{ background: "#d4d4d4" }}
+            />
+          </Form.Group>
 
-      <form onSubmit={handleSubmitRegister}>
-        <div className="form-outline mb-4">
-          <input
-            name="name"
-            value={username}
-            onChange={e =>
-              setUsername(e.target.value)
-            }
-            type="text"
-            id="form3Example1cg"
-            className="form-control form-control-lg"
-          />
-          <label
-            className="form-lable fw-bold"
-            htmlFor="form3Example1cg">
-            Your Name
-          </label>
-        </div>
+          <Form.Group className="mb-3" controlId="email">
+            <Form.Label className="green fw-bold">Your Email</Form.Label>
+            <Form.Control
+              name="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              type="email"
+              className="rounded-0 name"
+              style={{ background: "#d4d4d4" }}
+            />
+          </Form.Group>
 
-        <div className="form-outline mb-4">
-          <input
-            name="email"
-            value={email}
-            onChange={e =>
-              setEmail(e.target.value)
-            }
-            type="email"
-            id="form3Example3cg"
-            className="form-control form-control-lg"
-          />
-          <label
-            className="form-lable fw-bold"
-            htmlFor="form3Example3cg">
-            Your Email
-          </label>
-        </div>
+          <Form.Group className="mb-3" controlId="password">
+            <Form.Label className="green fw-bold">Password</Form.Label>
+            <Form.Control
+              name="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              type="password"
+              className="rounded-0 name"
+              style={{ background: "#d4d4d4" }}
+            />
+          </Form.Group>
 
-        <div className="form-outline mb-4">
-          <input
-            name="password"
-            value={password}
-            onChange={e =>
-              setPassword(e.target.value)
-            }
-            type="password"
-            id="form3Example4cg"
-            className="form-control form-control-lg"
-          />
-          <label
-            className="form-lable fw-bold"
-            htmlFor="form3Example4cg">
-            Password
-          </label>
-        </div>
-
-        <div>
-          <Button
-            className="
-          bg-dark green fw-bold
-           p-1 border border-info rounded"
-            type="submit">
-            Register
+          <Button type="submit" id="contactbtn" className="py-2 px-5">
+            Login
           </Button>
-        </div>
-
-        <div className=" d-flex">
-          <p>Have already an account?</p>
-          <Link
-            to="/Login"
-            className="d4d4 register_link ms-2 fw-bold text-decoration-none">
-            Login here
-          </Link>
-        </div>
-      </form>
+          <p className="mt-5 text-center">
+            Have already an account?
+            <Link
+              to="/Login"
+              className="d4d4 register_link ms-2 fw-bold text-decoration-none"
+            >
+              Login here
+            </Link>
+          </p>
+        </Form>
+      </div>
     </div>
   );
 }
