66c4f7bcba34a24a61e50a7291b16357cc8d89b9
Kevin14Tsui <lungtsui15@gmial.com>
2023-03-03 18:18:17 +0800

    post-id 225642

--- my_app/src/Post/src/PostMinLayout.js
+++ my_app/src/Post/src/PostMinLayout.js
@@ -1,6 +1,6 @@
-import { Link } from 'react-router-dom';
-import Table from 'react-bootstrap/Table';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { Link } from "react-router-dom";
+import Table from "react-bootstrap/Table";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function Post({
   _id,
@@ -15,46 +15,41 @@ export default function Post({
       <div
         className="container-fluid text-center text-wrap"
         style={{
-          marginBottom: '100px',
-          background: '#000',
-        }}>
+          marginBottom: "100px",
+          background: "#000",
+        }}
+      >
         <tr
           className=" row w-100  align-items-center"
           style={{
-            borderBottom: ' #04b6163b solid 1px',
-          }}>
+            borderBottom: " #04b6163b solid 1px",
+          }}
+        >
           <td className="col-2 p-md-3 d-none d-md-block smallborder">
-            <Link>
-              <img
-                alt=""
-                className="icon"
-              />
+            <Link to={`/post/${_id}`}>
+              <img alt="" className="icon" />
             </Link>
           </td>
           <td className="text-start col-md-8 px-3 px-sm-5 smallborder">
-            <Link className="text-decoration-none">
+            <Link to={`/post/${_id}`} className="text-decoration-none">
               <p
                 className="green 
               fs-5 m-0 title d-inline-block"
                 style={{
-                  wordBreak: 'break-word',
-                }}>
+                  wordBreak: "break-word",
+                }}
+              >
                 {title}
               </p>
             </Link>
-            <p className="fw-light d4d4 m-0">
-              - Joanna
-            </p>
+            <p className="fw-light d4d4 m-0">- Joanna</p>
           </td>
           <td
             className="col-2 p-md-3 text-start 
-          d-none d-md-block">
-            <p className="fw-light d4d4 m-0">
-              Comments: 2k
-            </p>
-            <p className="fw-light d4d4">
-              {createdAt}
-            </p>
+          d-none d-md-block"
+          >
+            <p className="fw-light d4d4 m-0">Comments: 2k</p>
+            <p className="fw-light d4d4">{createdAt}</p>
           </td>
         </tr>
       </div>
