554afd13dce90bb51750401c966947a01355e65f
Joanna <Joanna@fakemail.com>
2023-03-06 13:06:32 +0800

    edit comment css

--- api/index.js
+++ api/index.js
@@ -1,46 +1,43 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-const mongoose = require('mongoose');
-const User = require('./models/User');
-const Post = require('./models/Post');
-const Comment = require('./models/Comment');
-const jwt = require('jsonwebtoken');
+const cors = require("cors");
+const mongoose = require("mongoose");
+const User = require("./models/User");
+const Post = require("./models/Post");
+const Comment = require("./models/Comment");
+const jwt = require("jsonwebtoken");
 
-const multer = require('multer');
+const multer = require("multer");
 
 const uploadMiddleware = multer({
-  dest: 'uploads/',
+  dest: "uploads/",
 });
 
 const commentMiddleware = multer({
-  dest: 'commentUpload/',
+  dest: "commentUpload/",
 });
-const fs = require('fs');
+const fs = require("fs");
 
-const cookieParser = require('cookie-parser');
+const cookieParser = require("cookie-parser");
 
-const bcrypt = require('bcryptjs'); //for hashing password
+const bcrypt = require("bcryptjs"); //for hashing password
 var salt = bcrypt.genSaltSync(10); // salt= to the hashed password
-const secret = 'sdsgdfgzdrgzrdgd4g5f4fgs5dvd6';
+const secret = "sdsgdfgzdrgzrdgd4g5f4fgs5dvd6";
 app.use(
   cors({
     credentials: true,
-    origin: '*', //'http://localhost:3000',
-  }),
+    origin: "http://localhost:3000",
+  })
 );
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(
-  '/uploads',
-  express.static(__dirname + '/uploads'),
-);
+app.use("/uploads", express.static(__dirname + "/uploads"));
 mongoose.connect(
-  'mongodb+srv://Stephanie:eMsZpNLBcgSfhtxI@cluster0.bp39mje.mongodb.net/?retryWrites=true&w=majority',
+  "mongodb+srv://Stephanie:eMsZpNLBcgSfhtxI@cluster0.bp39mje.mongodb.net/?retryWrites=true&w=majority"
 );
 
-app.post('/register', async (req, res) => {
+app.post("/register", async (req, res) => {
   const { username, password, email } = req.body;
   try {
     const userDoc = await User.create({
@@ -57,14 +54,11 @@ app.post('/register', async (req, res) => {
   // res.json('test 4000');
 });
 
-app.post('/login', async (req, res) => {
+app.post("/login", async (req, res) => {
   const { email, password } = req.body;
   const userDoc = await User.findOne({ email });
 
-  const passOk = bcrypt.compareSync(
-    password,
-    userDoc.password,
-  );
+  const passOk = bcrypt.compareSync(password, userDoc.password);
   if (passOk) {
     // logged in
     jwt.sign(
@@ -76,18 +70,18 @@ app.post('/login', async (req, res) => {
       {},
       (err, token) => {
         if (err) throw err;
-        res.cookie('token', token).json({
+        res.cookie("token", token).json({
           id: userDoc._id,
           username: userDoc.username,
         });
-      },
+      }
     );
   } else {
-    res.status(400).json('wrong credentials');
+    res.status(400).json("wrong credentials");
   }
 });
 
-app.get('/proflie', (req, res) => {
+app.get("/proflie", (req, res) => {
   const { token } = req.cookies;
   jwt.verify(token, secret, {}, (err, info) => {
     if (err) throw err;
@@ -95,147 +89,110 @@ app.get('/proflie', (req, res) => {
   });
 });
 
-app.post('/logout', (req, res) => {
-  res.cookie('token', '').json('ok');
+app.post("/logout", (req, res) => {
+  res.cookie("token", "").json("ok");
 });
 
-app.post(
-  '/createpost',
-  uploadMiddleware.single('file'),
-  async (req, res) => {
-    const { originalname, path } = req.file;
-    const parts = originalname.split('.');
-    const ext = parts[parts.length - 1];
-    const newPath = path + '.' + ext;
-    fs.renameSync(path, newPath);
+app.post("/createpost", uploadMiddleware.single("file"), async (req, res) => {
+  const { originalname, path } = req.file;
+  const parts = originalname.split(".");
+  const ext = parts[parts.length - 1];
+  const newPath = path + "." + ext;
+  fs.renameSync(path, newPath);
 
-    const { token } = req.cookies;
-    jwt.verify(
-      token,
-      secret,
-      {},
-      async (err, info) => {
-        if (err) throw err;
-        const { title, summary, content } =
-          req.body;
-        const postDoc = await Post.create({
-          title,
-          summary,
-          content,
-          cover: newPath,
-          author: info.id,
-          comments: [],
-        });
-        res.json(postDoc);
-      },
-    );
-  },
-); /// collect the data from the create post form
+  const { token } = req.cookies;
+  jwt.verify(token, secret, {}, async (err, info) => {
+    if (err) throw err;
+    const { title, summary, content } = req.body;
+    const postDoc = await Post.create({
+      title,
+      summary,
+      content,
+      cover: newPath,
+      author: info.id,
+      comments: [],
+    });
+    res.json(postDoc);
+  });
+}); /// collect the data from the create post form
 
 //put the new data to post
-app.put(
-  '/post',
-  uploadMiddleware.single('file'),
-  async (req, res) => {
-    let newPath = null;
-    if (req.file) {
-      const { originalname, path } = req.file;
-      const parts = originalname.split('.');
-      const ext = parts[parts.length - 1];
-      newPath = path + '.' + ext;
-      fs.renameSync(path, newPath);
+app.put("/post", uploadMiddleware.single("file"), async (req, res) => {
+  let newPath = null;
+  if (req.file) {
+    const { originalname, path } = req.file;
+    const parts = originalname.split(".");
+    const ext = parts[parts.length - 1];
+    newPath = path + "." + ext;
+    fs.renameSync(path, newPath);
+  }
+
+  const { token } = req.cookies;
+  jwt.verify(token, secret, {}, async (err, info) => {
+    if (err) throw err;
+    const { id, title, summary, content } = req.body;
+    const postDoc = await Post.findById(id);
+    const IsAuthor = JSON.stringify(postDoc.author) === JSON.stringify(info.id); // check author is right before update
+    if (!IsAuthor) {
+      return res.status(400).json("you are not the author");
     }
+    await postDoc.update({
+      title,
+      summary,
+      content,
+      cover: newPath ? newPath : postDoc.cover,
+    }); // updata content
+    res.json(postDoc);
+  });
+});
 
-    const { token } = req.cookies;
-    jwt.verify(
-      token,
-      secret,
-      {},
-      async (err, info) => {
-        if (err) throw err;
-        const { id, title, summary, content } =
-          req.body;
-        const postDoc = await Post.findById(id);
-        const IsAuthor =
-          JSON.stringify(postDoc.author) ===
-          JSON.stringify(info.id); // check author is right before update
-        if (!IsAuthor) {
-          return res
-            .status(400)
-            .json('you are not the author');
-        }
-        await postDoc.update({
-          title,
-          summary,
-          content,
-          cover: newPath
-            ? newPath
-            : postDoc.cover,
-        }); // updata content
-        res.json(postDoc);
-      },
-    );
-  },
-);
-
-app.get('/createpost', async (req, res) => {
+app.get("/createpost", async (req, res) => {
   res.json(
     await Post.find()
-      .populate('author', ['username'])
+      .populate("author", ["username"])
       .sort({ createAt: -1 })
-      .limit(20),
+      .limit(20)
   );
 });
 
 // to PostPage
-app.get('/post/:id', async (req, res) => {
+app.get("/post/:id", async (req, res) => {
   const { id } = req.params;
-  const postDoc = await Post.findById(
-    id,
-  ).populate('author', ['username']);
+  const postDoc = await Post.findById(id).populate("author", ["username"]);
   //select what info you need
   res.json(postDoc); //get the author name though the :id URL with req.params;
 });
 
 app.post(
-  '/post/:id/comment',
-  commentMiddleware.single('file'),
+  "/post/:id/comment",
+  commentMiddleware.single("file"),
   async (req, res) => {
     const { id } = req.params;
     const { token } = req.cookies;
-    jwt.verify(
-      token,
-      secret,
-      {},
-      async (err, info) => {
-        if (err) throw err;
+    jwt.verify(token, secret, {}, async (err, info) => {
+      if (err) throw err;
 
-        const { comment } = req.body;
-        const commentDoc = await Comment.create({
-          postId: id,
-          comment,
-          author: info.username, //collect the name of the user who made the comment form the Jwt,sign Token
-        });
+      const { comment } = req.body;
+      const commentDoc = await Comment.create({
+        postId: id,
+        comment,
+        author: info.username, //collect the name of the user who made the comment form the Jwt,sign Token
+      });
 
-        res.json(commentDoc);
-        // const postComment = Post.findById(
-        //   id,
-        // ).populate('comments.commentDoc');  // have to kept researching for the per post per comments
-        // console.log(commentDoc);
-        // console.log(postComment);
-      },
-    ); //here we create new comment post
+      res.json(commentDoc);
+      // const postComment = Post.findById(
+      //   id,
+      // ).populate('comments.commentDoc');  // have to kept researching for the per post per comments
+      // console.log(commentDoc);
+      // console.log(postComment);
+    }); //here we create new comment post
 
     // console.log(id);
-  },
+  }
 );
 
-app.get('/post/:id/comment', async (req, res) => {
-  res.json(
-    await Comment.find()
-      .sort({ createAt: -1 })
-      .limit(20),
-  );
+app.get("/post/:id/comment", async (req, res) => {
+  res.json(await Comment.find().sort({ createAt: -1 }).limit(20));
 }); //here we put the commentPost on the mini PostPage Layout
 
 app.listen(4000);
--- my_app/src/PostPage.js
+++ my_app/src/PostPage.js
@@ -1,19 +1,17 @@
-import {
-  useEffect,
-  useState,
-  useContext,
-} from 'react';
-import {
-  Link,
-  useParams,
-} from 'react-router-dom';
-import { formatISO9075 } from 'date-fns';
-import Container from 'react-bootstrap/Container';
-import './Postbox.css';
-import { UserContext } from './UserContext';
-import Button from 'react-bootstrap/esm/Button';
-import CommentForm from './CommentForm';
-import Comment from './Post/src/commetMinilayout';
+import { useEffect, useState, useContext } from "react";
+import { Link, useParams } from "react-router-dom";
+import { formatISO9075 } from "date-fns";
+import Container from "react-bootstrap/Container";
+import "./Postbox.css";
+import { UserContext } from "./UserContext";
+import CommentForm from "./CommentForm";
+import Comment from "./Post/src/commetMinilayout";
+import CloseIcon from "@mui/icons-material/Close";
+import Crop75Icon from "@mui/icons-material/Crop75";
+import HorizontalRuleIcon from "@mui/icons-material/HorizontalRule";
+import DeleteIcon from "@mui/icons-material/Delete";
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+import Tooltip from "react-bootstrap/Tooltip";
 
 export default function PostPage() {
   const [postInfo, setPostInfo] = useState(null);
@@ -21,13 +19,11 @@ export default function PostPage() {
   const { id } = useParams();
 
   const [comments, setComments] = useState([]); //set all the comments by useParmas()
- 
+
   //    catch data
   useEffect(() => {
-    fetch(
-      `http://localhost:4000/post/${id}`,
-    ).then(response => {
-      response.json().then(postInfo => {
+    fetch(`http://localhost:4000/post/${id}`).then((response) => {
+      response.json().then((postInfo) => {
         setPostInfo(postInfo);
         // console.log(postInfo._id);
       });
@@ -35,78 +31,97 @@ export default function PostPage() {
   }, []);
 
   useEffect(() => {
-    fetch(
-      `http://localhost:4000/post/${id}/comment`,
-    ).then(response => {
-      response.json().then(comments => {
-       
+    fetch(`http://localhost:4000/post/${id}/comment`).then((response) => {
+      response.json().then((comments) => {
         setComments(comments);
-        
       });
     });
   }, []);
 
-  if (!postInfo) return '';
+  if (!postInfo) return "";
 
   return (
     <Container className="post-page ">
-      <h1 className=" text-light ">
-        {postInfo.title}
-      </h1>
-
-      <div className="text-light ">
-        by {postInfo.author.username}{' '}
-        {formatISO9075(
-          new Date(postInfo.createdAt),
-        )}
-      </div>
-      {userInfo.id === postInfo.author._id && (
-        <div className="text-light post-page-content">
-          <Link
-            className="py-2 px-3 my-2 "
-            to={`/edit/${postInfo._id}`}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-6 h-6 px-1"
-              style={{ height: '20px' }}>
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
-              />
-            </svg>
-            Edit this post
-          </Link>
-        </div>
-      )}
-      <div className="post-page-cover">
-        <img
-          src={`http://localhost:4000/${postInfo.cover}`}
-        />
-      </div>
       <div
-        style={{ objectFit: 'scale-down' }}
-        className="text-light  post-page-content"
-        dangerouslySetInnerHTML={{
-          __html: postInfo.content,
-        }}
-      />
-
-      <CommentForm />
-
-      <div>
-        {comments.length > 0 &&
-          comments
-            .filter(
-              comment => comment.postId === id,
-            )
-            .map(comments => (
-              <Comment {...comments} /> // map out the comments
-            ))}
+        className="container-fluid  d4d4 text-center pt-5 mt-2 pb-5"
+        style={{ maxWidth: "1000px" }}
+      >
+        <article className="artcontent">
+          <h2 className="fw-bold mb-3">{postInfo.title}</h2>
+          <p className="green mb-5">
+            Asked {formatISO9075(new Date(postInfo.createdAt))}
+            <br />
+            {userInfo.id === postInfo.author._id && (
+              <OverlayTrigger
+                key="right"
+                placement="right"
+                overlay={<Tooltip id="right">Edit This Post</Tooltip>}
+              >
+                <span className="edit">
+                  <Link
+                    to={`/edit/${postInfo._id}`}
+                    style={{ color: "rgb(199, 199, 0)" }}
+                    className="fw-bold text-decoration-none edittext"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="w-6 h-6 px-1 "
+                      style={{ height: "20px", color: "rgb(199, 199, 0)" }}
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
+                      />
+                    </svg>
+                    Edit
+                  </Link>
+                </span>
+              </OverlayTrigger>
+            )}
+          </p>
+          <div
+            className=" text-center position-relative py-1"
+            style={{
+              border: "#04b616 dashed 2px",
+              color: "#04b616",
+            }}
+          >
+            <p className="m-0">
+              {"< Written by"}{" "}
+              <span className="d4d4 fw-bold">{postInfo.author.username}</span>
+              {" /> "}
+              <span className="position-absolute end-0 pe-3 d-none d-sm-inline">
+                <HorizontalRuleIcon /> <Crop75Icon /> <CloseIcon />
+              </span>
+            </p>
+          </div>
+          <div className="blogcontainer">
+            <div
+              className="text-start p-4 postbody"
+              dangerouslySetInnerHTML={{
+                __html: postInfo.content,
+              }}
+            ></div>
+            <p className="text-center"> -END-</p>
+          </div>
+        </article>
+        <h3 style={{ Color: "white" }} className="my-5">
+          Comment
+        </h3>
+        <div>
+          {comments.length > 0 &&
+            comments
+              .filter((comment) => comment.postId === id)
+              .map((comments) => (
+                <Comment {...comments} /> // map out the comments
+              ))}
+        </div>
+        <CommentForm />
       </div>
     </Container>
   );
