644180fd90497068107e53d903734d9dde1e1d52
Joanna <Joanna@fakemail.com>
2023-03-06 08:46:49 +0800

    add link to the card

--- my_app/src/Card.js
+++ my_app/src/Card.js
@@ -5,6 +5,7 @@ import card03 from "./img/card03.png";
 import card04 from "./img/card04.png";
 import card05 from "./img/card05.png";
 import card06 from "./img/card06.png";
+import { Link } from "react-router-dom";
 import "./Card.css";
 
 function Cards() {
@@ -36,16 +37,21 @@ function Cards() {
               />
             </div>
             <Card.Body style={{ borderTop: "#04b616 solid 3px" }}>
-              <Card.Title className="green fw-bold fs-4 my-4">
-                AI and Machine Learning
-              </Card.Title>
+              <Link
+                to={`https://netnordic.com/what-we-do/multi-cloud-application-enablement/ai-and-machine-learning/`}
+                className="text-decoration-none"
+                target="_blank"
+              >
+                <Card.Title className="green fw-bold fs-4 my-4">
+                  AI and Machine Learning
+                </Card.Title>
+              </Link>
               <Card.Text className="d4d4 fw-light">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
-                eget metus faucibus, porttitor eros in, porta ...
+                There are more gains to be made with AI and machine learning,
+                right now! We can support your journey into the future with
+                everything...
               </Card.Text>
-              <p className="mt-3 fs-6 fw-bold text-light">
-                BY Joanna@Yau 23/02/2023
-              </p>
+              <p className="mt-3 fs-6 fw-bold text-light">From Netnordic</p>
             </Card.Body>
           </Card>
         </div>
@@ -73,16 +79,20 @@ function Cards() {
               />
             </div>
             <Card.Body style={{ borderTop: "#04b616 solid 3px" }}>
-              <Card.Title className="green fw-bold fs-4 my-4">
-                Types of Coding Languages Guide
-              </Card.Title>
+              <Link
+                to={`https://sphero.com/blogs/news/types-of-coding-languages`}
+                className="text-decoration-none"
+                target="_blank"
+              >
+                <Card.Title className="green fw-bold fs-4 my-4 cardlink">
+                  Types of Coding Languages Guide
+                </Card.Title>
+              </Link>
               <Card.Text className="d4d4 fw-light">
-                Lorem ipsum dolor sit amet, consectetur faucibus, porttitor eros
-                in, porta ...
+                With hundreds of coding languages in existence, it can feel
+                overwhelming to break into the world of computer programming ...
               </Card.Text>
-              <p className="mt-3 fs-6 fw-bold text-light">
-                BY Kevin 19/02/2023
-              </p>
+              <p className="mt-3 fs-6 fw-bold text-light">From Sphero.com</p>
             </Card.Body>
           </Card>
         </div>
@@ -110,15 +120,21 @@ function Cards() {
               />
             </div>
             <Card.Body style={{ borderTop: "#04b616 solid 3px" }}>
-              <Card.Title className="green fw-bold fs-4 my-4">
-                Best Coding Games for Kids
-              </Card.Title>
+              <Link
+                to={`https://www.create-learn.us/blog/best-coding-games-for-kids/`}
+                className="text-decoration-none"
+                target="_blank"
+              >
+                <Card.Title className="green fw-bold fs-4 my-4 cardlink">
+                  Best Coding Games for Kids
+                </Card.Title>
+              </Link>
               <Card.Text className="d4d4 fw-light">
-                Lorem ipsum dolor sit amet, consectetur faucibus, porttitor eros
-                in, porta ...
+                Learning to code will be challenging for everyone, but a growth
+                mindset and having fun are the keys to building habits of ...
               </Card.Text>
               <p className="mt-3 fs-6 fw-bold text-light">
-                BY Stephanie 19/02/2023
+                From Create & Learn
               </p>
             </Card.Body>
           </Card>
@@ -147,16 +163,21 @@ function Cards() {
               />
             </div>
             <Card.Body style={{ borderTop: "#04b616 solid 3px" }}>
-              <Card.Title className="green fw-bold fs-4 my-4">
-                25 Best Coding Apps for Kids
-              </Card.Title>
+              <Link
+                to={`https://www.codewizardshq.com/coding-apps-for-kids/`}
+                className="text-decoration-none"
+                target="_blank"
+              >
+                <Card.Title className="green fw-bold fs-4 my-4 cardlink">
+                  25 Best Coding Apps for Kids
+                </Card.Title>
+              </Link>
               <Card.Text className="d4d4 fw-light">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
-                eget metus faucibus, porttitor eros in, porta ...
+                As coding becomes a crucial element of childhood education,
+                parents are looking for different ways to reinforce coding
+                concepts...
               </Card.Text>
-              <p className="mt-3 fs-6 fw-bold text-light">
-                BY Joanna@Yau 18/02/202
-              </p>
+              <p className="mt-3 fs-6 fw-bold text-light">From CodeWizardsHQ</p>
             </Card.Body>
           </Card>
         </div>
@@ -184,16 +205,21 @@ function Cards() {
               />
             </div>
             <Card.Body style={{ borderTop: "#04b616 solid 3px" }}>
-              <Card.Title className="green fw-bold fs-4 my-4">
-                Android Apps by Coding on Google Play
-              </Card.Title>
+              <Link
+                to={`https://richardsondx.medium.com/where-to-start-if-you-don-t-know-anything-about-web-development-and-coding-ac3374b85b94`}
+                className="text-decoration-none"
+                target="_blank"
+              >
+                <Card.Title className="green fw-bold fs-4 my-4 cardlink">
+                  Where to learn HTML and CSS and build your first website
+                </Card.Title>
+              </Link>
               <Card.Text className="d4d4 fw-light">
-                faucibus, porttitor eros in, porta faucibus, porttitor eros in,
-                porta...
+                I’ve been receiving a lot of emails from readers and some of my
+                followers about the subject. Although they found my article “How
+                ...
               </Card.Text>
-              <p className="mt-3 fs-6 fw-bold text-light">
-                BY Kevin 16/02/2023
-              </p>
+              <p className="mt-3 fs-6 fw-bold text-light">From Medium</p>
             </Card.Body>
           </Card>
         </div>
@@ -221,16 +247,21 @@ function Cards() {
               />
             </div>
             <Card.Body style={{ borderTop: "#04b616 solid 3px" }}>
-              <Card.Title className="green fw-bold fs-4 my-4">
-                Coding for Journalists
-              </Card.Title>
+              <Link
+                to={`https://mediablog.prnewswire.com/2022/07/06/coding-for-journalists/`}
+                className="text-decoration-none"
+                target="_blank"
+              >
+                <Card.Title className="green fw-bold fs-4 my-4 cardlink">
+                  Coding for Journalists
+                </Card.Title>
+              </Link>
               <Card.Text className="d4d4 fw-light">
-                Lorem ipsum dolor sit amet, consectetur faucibus, porttitor eros
-                in, porta ...
+                Is there a story anymore that doesn’t include some sort of data?
+                Whether you’re reporting on the pandemic, weather, crime trends,
+                ...
               </Card.Text>
-              <p className="mt-3 fs-6 fw-bold text-light">
-                BY Stephanie 13/02/2023
-              </p>
+              <p className="mt-3 fs-6 fw-bold text-light">From CISION</p>
             </Card.Body>
           </Card>
         </div>
